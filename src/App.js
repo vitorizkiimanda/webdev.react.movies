f03100001685e18ddb7a7ef20991e4a57289acb2
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2021-02-04 22:57:00 +0700

    basic UI listing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import {
+    CircularProgressbarWithChildren,
+    buildStyles,
+} from "react-circular-progressbar";
+
 export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+
+        this.dummy = new Array(100).fill(true);
+
+        console.log(this.dummy);
     }
 
     componentDidMount() {}
@@ -14,21 +23,46 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div className="container">
+                {this.dummy.map((val, index) => {
+                    return (
+                        <div className="card">
+                            <img
+                                src="https://www.themoviedb.org/t/p/w440_and_h660_face/hm58Jw4Lw8OIeECIq5qyPYhAeRJ.jpg"
+                                className="card-img"
+                                alt=""
+                            />
+                            <div className="card-text-container">
+                                <div className="rating-container">
+                                    <CircularProgressbarWithChildren
+                                        value={7.3}
+                                        maxValue={10}
+                                        strokeWidth={6}
+                                        background
+                                        backgroundPadding={6}
+                                        styles={buildStyles({
+                                            strokeLinecap: "round",
+                                            trailColor: "rgba(35,208,122, 0.3)",
+                                            pathColor: "rgba(35,208,122, 1)",
+                                            backgroundColor: "#091c23",
+                                        })}
+                                    >
+                                        <div className="rating-text-container">
+                                            <strong className="rating-text-strong">
+                                                73
+                                            </strong>
+                                            %
+                                        </div>
+                                    </CircularProgressbarWithChildren>
+                                </div>
+                                <div className="text-title">Title</div>
+                                <div className="text-release-date">
+                                    Release Date
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
